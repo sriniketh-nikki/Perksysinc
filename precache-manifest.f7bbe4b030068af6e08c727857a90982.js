6ac26024971b02891607525797e85c2cb380f16f
srikanthdpk <dpksrikanth95@gmail.com>
2023-08-30 18:48:38 +0530

    Updates

---
+++ precache-manifest.f7bbe4b030068af6e08c727857a90982.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "41afb5272d2d6b1cb1c7",
+    "url": "/Perksysinc/static/css/main.44da0e01.chunk.css"
+  },
+  {
+    "revision": "41afb5272d2d6b1cb1c7",
+    "url": "/Perksysinc/static/js/main.41afb527.chunk.js"
+  },
+  {
+    "revision": "561126de61029355b316",
+    "url": "/Perksysinc/static/js/1.561126de.chunk.js"
+  },
+  {
+    "revision": "e94101dfaf1b30200679",
+    "url": "/Perksysinc/static/css/2.35f42551.chunk.css"
+  },
+  {
+    "revision": "e94101dfaf1b30200679",
+    "url": "/Perksysinc/static/js/2.e94101df.chunk.js"
+  },
+  {
+    "revision": "e08ce6247b1f65785667",
+    "url": "/Perksysinc/static/js/runtime~main.e08ce624.js"
+  },
+  {
+    "revision": "6d428949e6f44f6418e4d6b9747bb7b1",
+    "url": "/Perksysinc/static/media/computer.6d428949.jpg"
+  },
+  {
+    "revision": "febff13477f163fd451498f5021ec4c4",
+    "url": "/Perksysinc/static/media/abt.febff134.jfif"
+  },
+  {
+    "revision": "b14312074ebe4dd23c32ddf92050e9ae",
+    "url": "/Perksysinc/static/media/about-us-banner.b1431207.png"
+  },
+  {
+    "revision": "7510e55f9c5c37972324a65682c380a0",
+    "url": "/Perksysinc/static/media/abt_perksysinc.7510e55f.jpg"
+  },
+  {
+    "revision": "19d24ed23faccf86829fc65512508c5c",
+    "url": "/Perksysinc/static/media/con1.19d24ed2.jpg"
+  },
+  {
+    "revision": "f0b5a69c1474c3a1bcfb45e2413ba773",
+    "url": "/Perksysinc/static/media/con2.f0b5a69c.png"
+  },
+  {
+    "revision": "36c95c148036706f2fbde8c6948968f5",
+    "url": "/Perksysinc/static/media/servlogo.36c95c14.jpg"
+  },
+  {
+    "revision": "5eea08860d9544505b09573b4c69b6ba",
+    "url": "/Perksysinc/static/media/serlogo1.5eea0886.jfif"
+  },
+  {
+    "revision": "86d46bae25c18bc1860565132d488f91",
+    "url": "/Perksysinc/static/media/services.86d46bae.jpg"
+  },
+  {
+    "revision": "df822c0f326a31f5f8bfd168a3f94d83",
+    "url": "/Perksysinc/static/media/client.df822c0f.jfif"
+  },
+  {
+    "revision": "8ec53405dfa56fcf7db737ab0ff2302d",
+    "url": "/Perksysinc/static/media/client1.8ec53405.jfif"
+  },
+  {
+    "revision": "a30fb81bd52143bcd4de2898422ac8b9",
+    "url": "/Perksysinc/static/media/bootstrap-icons.a30fb81b.woff2"
+  },
+  {
+    "revision": "3d0ec57f40d62ba6a8e0d1857bb8f7d5",
+    "url": "/Perksysinc/static/media/bootstrap-icons.3d0ec57f.woff"
+  },
+  {
+    "revision": "ea3f91125baa9fb03db33b0e7579610a",
+    "url": "/Perksysinc/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.ae3c2bfe3533e30d854e4dee8c773e55.js"
+  "/Perksysinc/precache-manifest.f7bbe4b030068af6e08c727857a90982.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-gh-pages/index.html", {
+workbox.routing.registerNavigationRoute("/Perksysinc/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
